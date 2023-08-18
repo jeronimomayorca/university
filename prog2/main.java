f7c175b2f723f9db62570fc89d754eff7e439d27
Jeronimo <mayorcajeronimo@gmail.com>
2023-08-18 08:38:57 -0500

    add exercises

---
+++ prog2/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static PrintStream screen = System.out;
+    public static Scanner keyboard = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        int sum = 0;
+
+        screen.println("Ingrese un numero");
+        num1 = keyboard.nextInt();
+
+
+    }
+}
+
