9852d424b03d160587ad2fd87675786a84fbb225
Jeronimo <mayorcajeronimo@gmail.com>
2023-09-18 11:37:40 -0500

    add exercises and new functions

--- prog2/src/main.java
+++ prog2/src/main.java
@@ -1,28 +1,161 @@
-import java.io.PrintStream;
-import java.util.Scanner;
-
-import static functions.isMultiple.isMultiple;
+import java.awt.*;
+import java.util.Arrays;
 
 public class main {
-    public static PrintStream screen = System.out;
-    public static Scanner keyboard = new Scanner(System.in);
-    public static void main(String[]args){
-        int num1=0;
-        int num2=0;
-        boolean isMultiple=false;
-        screen.println("ingrese el primer numero");
-        num1= keyboard.nextInt();
-        screen.println("ingrese el segundo numero");
-        num2= keyboard.nextInt();
 
-        //isMultiple=multiplos.isMultiple(num1,num2);
+    public static final int COLOR_YELLOW = new Color(250, 201, 20).getRGB();
+    public static final int COLOR_BLUE = new Color(17, 13, 99).getRGB();
+    public static final int COLOR_RED = new Color(196, 0, 15).getRGB();
+    public static final int COLOR_BLACK = new Color(5, 5, 5).getRGB();
+    public static final int COLOR_WHITE = new Color(255, 255, 255).getRGB();
+    public static final int COLOR_GREEN = new Color(35, 138, 51).getRGB();
 
-        if(isMultiple(num1,num2))
-            screen.println(num1 +" es un multiplo de "+num2);
-        else
-            screen.println(num1+" No es multiplo de "+num2);
 
+
+    public static int [][] createCOLFlag(int height, int width){
+        int [][] flag = new int[height][width];
+        int rowIni = 0;
+        int rowEnd = 0;
+
+        rowIni = 0;
+        rowEnd = (int)(height*0.50);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            for (int cell = 0; cell < flag[row].length; cell++){
+                flag[row][cell] = COLOR_YELLOW;
+            }
+        }
+
+        rowIni = (int)(height*0.50);
+        rowEnd = (int)(height*0.75);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            for (int cell = 0; cell < flag[row].length; cell++){
+                flag[row][cell] = COLOR_BLUE;
+            }
+        }
+
+        rowIni = (int)(height*0.75);
+        rowEnd = (int)(height);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            for (int cell = 0; cell < flag[row].length; cell++){
+                flag[row][cell] = COLOR_RED;
+            }
+        }
+
+
+
+        return flag;
     }
+
+
+    public static int [][] createVENFlag(int height, int width){
+        int [][] flag = new int[height][width];
+        int rowIni = 0;
+        int rowEnd = 0;
+
+        rowEnd = (int)(height*0.33);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            Arrays.fill(flag[row], COLOR_YELLOW);
+        }
+
+        rowIni = (int)(height*0.33);
+        rowEnd = (int)(height*0.66);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            Arrays.fill(flag[row], COLOR_BLUE);
+        }
+
+        rowIni = (int)(height*0.66);
+        rowEnd = (int)(height);
+
+        for (int row = rowIni; row < rowEnd; row++){
+            Arrays.fill(flag[row], COLOR_RED);
+        }
+
+
+
+        return flag;
+    }
+    public static void main(String[] args) {
+        int [][] flag = {};
+
+        // COLOMBIAN FLAG
+
+        flag = createCOLFlag(12,20);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createCOLFlag(12, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createCOLFlag(24, 20 );
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createCOLFlag(24, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        //VENEZUELAN FLAG
+
+        flag = createVENFlag(12,20);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(12, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 20 );
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+
+        //POLAND FLAG
+
+        flag = createPOLFlag(12,20);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(12, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 20 );
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+
+        //VENEZUELAN FLAG
+
+        flag = createVENFlag(12,20);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(12, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 20 );
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+
+        //VENEZUELAN FLAG
+
+        flag = createVENFlag(12,20);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(12, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 20 );
+        JOptionPaneArrays.showColorArray2D(null, flag);
+
+        flag = createVENFlag(24, 40);
+        JOptionPaneArrays.showColorArray2D(null, flag);
+    }
+
 
 
 }
